c7582025c07745d7c275aed98b242748e5e2a37d
San Vert <itsanvert@gmail.com>
2024-10-05 00:36:43 +0700

    initial commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision"; // Adjust the import path accordingly
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
 
 const Hero = () => {
   return (
@@ -16,15 +17,29 @@ const Hero = () => {
         />
         <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.5] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vh] flex flex-col items-center justify-center">
-          <h2 className="uppercase -tracking-widest text-xs text-center text-blue-100 max-w-80">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.
           </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 tet-sm md:text-lg lg:text-2xl">
+            Hi, I'm Vert, a Next.js Developer based in Cambodia 👋
+          </p>
+          <a href="#about">
+            <MagicButton 
+            title='Show my work'
+            
+             />
+          </a>
         </div>
       </div>
     </div>
