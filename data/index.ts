5166cd345a1b7b026f0ca0f9617bf11b154d00f2
San Vert <itsanvert@gmail.com>
2024-10-08 20:17:56 +0700

    initail commit

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export const GridItems = [
+    { title: "Title1", description: "Des1", id: 1 },
+    { title: "Title1", description: "Des1", id: 1 },
+    { title: "Title1", description: "Des1", id: 1 },
+    { title: "Title1", description: "Des1", id: 1 },
+    { title: "Title1", description: "Des1", id: 1 },
+    { title: "Title1", description: "Des1", id: 1 },
+  ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Hero from "@/components/Hero";
-
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 import React from "react";
+import Grid from "@/components/ui/Grid";
 
 const page = () => {
   return (
-    <div className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
-    </div>
+    </main>
   );
 };
 
